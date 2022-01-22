9a29eff1a0873deeaccb485b8916ff301f3d7e36
mishellgc <mishell.gomez@unmsm.edu.pe>
2022-01-22 02:53:26 -0500

    c

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,22 @@
-from collections import namedtuple
-import altair as alt
-import math
+
+import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import pandas_datareader as data
+from tensorflow.keras.models import load_model
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+start = '2004-08-18'
+end = '2022-01-20'
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+st.title('Predicción de tendencia de acciones')
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+user_input = st.text_input('Introducir cotización bursátil' , 'GOOG')
 
-    points_per_turn = total_points / num_turns
+df = data.DataReader(user_input, 'yahoo', start, end)
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+# Describiendo los datos
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+st.subheader('Datos del 2004 al 2022') 
+st.write(df.describe())
